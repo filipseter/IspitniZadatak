dfef747dc7e289b5c45d137362d21e6af1b183f2
Filip <name@domain.com>
2016-11-26 20:23:23 +0100

    fourth commit

--- app/src/main/java/com/example/android/ispitnizadatak/AddActivity.java
+++ app/src/main/java/com/example/android/ispitnizadatak/AddActivity.java
@@ -5,7 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import java.sql.SQLException;
 
 /**
  * Created by android on 26.11.16..
@@ -19,6 +25,38 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         setToolbar();
+
+        final EditText note_name = ( EditText ) findViewById( R.id.edittext_insert_name );
+        final EditText note_details = ( EditText ) findViewById( R.id.edittext_insert_details );
+        Button save = ( Button ) findViewById( R.id.button_add_save );
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNoteToDatabase(
+                        note_name.getText().toString(),
+                        note_details.getText().toString()
+                );
+            }
+        });
+        Button cancel = ( Button ) findViewById( R.id.button_add_cancel );
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void saveNoteToDatabase(String note_name, String note_detail) {
+        Notes note = new Notes();
+        note.setName( note_name );
+        note.setDetails(note_detail);
+        try {
+            getDatabaseHelper().getNotesDao().create( note );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        onBackPressed();
     }
 
     private void setToolbar() {
@@ -52,4 +90,5 @@ public class AddActivity extends AppCompatActivity {
         }
         return databaseHelper;
     }
+
 }
